a610fe573847573bb7fb18618d9fd2f3afa216f1
Maria Isabel Lopez <mrlpz1515@gmail.com>
2021-01-06 18:11:07 -0500

    FEAT: First Working version

---
+++ lang.js
@@ -0,0 +1,47 @@
+const translations = {
+    en_us: {
+        title: "Vent with us!",
+        welcome: "You can vent with us",
+        description: "Tell me about a problem you have. Be as detailed as possible. This can be a big or small problem, global or personal. We will not share this information with anyone but we want to see if we can help you solve it.",
+        placeholder: "Vent here all you want",
+        send_btn: "Send",
+        new_problem: "I want to vent again!",
+        name: "Name",
+        final_title: "Thanks",
+        final_text: "I hope you feel better after sharing. Tell us ur email so we can tell you what can go next. We will not share this with anyone this is only for us to send you update about this product.",
+        final_knowmore: "Wanna stay up to date with this sruvey?",
+    },
+    es_pe: {
+        title: "Desahogate!",
+        welcome: "Desahogate con nosotros",
+        description: "Cuentame un problema que tengas. Se lo mas detallado/a posible. Este puede ser un problema grande o chico, global o personal. Esta informacion no la comportiremos con nadie mas solo queremos ver si podemos ayudarte a resolverlo.",
+        send_btn: "Enviar",
+        placeholder: "Escribe aqui lo que quieras",
+        new_problem: "Quiero escribir otro problema",
+        name: "Nombre",
+        final_title: "Grcaus",
+        final_text: "Espero que te sientas mejor luego de compartir. Coloca tu correo aqui para que puedas que te compartamos lo que viene. No vamos a compartir esta información.",
+        final_knowmore: "Quieres conocer los resultados?",
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("lang")){
+        const curr = localStorage.getItem("lang");
+        if (curr in translations){
+            changeLanguage(curr)
+        }
+    }
+    changeLanguage("en_us")
+});// set all the text
+
+function changeLanguage(newLang){
+    localStorage.setItem("lang", newLang)
+    let lang = translations[newLang]
+
+    document.title = lang.title;
+    document.getElementById("welcome").textContent = lang.welcome;document.getElementById("description").textContent = lang.description;
+    document.getElementById("problem").placeholder = lang.placeholder;
+    document.getElementById("send_btn").value = lang.send_btn;
+    document.getElementById("name").placeholder = lang.name;
+}
---
+++ success.php
@@ -0,0 +1,70 @@
+<?php 
+if($_SERVER['REQUEST_METHOD'] ==='POST'){
+    $to = "milg.style@gmail.com"; // this is your Email address
+    $from = "vent@mariaisabel.tech"; // this is the sender's Email address
+    $subject = $name;
+    $name = $_POST['name'];
+    $message = $_POST['problem'];
+
+    $headers = "From:" . $from;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="stylesheet" href="https://unpkg.com/wingcss"/>
+    <link rel="stylesheet" href="styles.css">
+    <script>
+    </script>
+    <title>Gracias por compartir!</title>
+</head>
+<body>
+    <script src="events.js"></script>
+
+    <main>
+    <div id="myModal" class="modal">
+
+        <!-- Modal content -->
+        <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>Some text in the Modal..</p>
+        </div>
+
+        </div>
+        <div>
+        <h1>Gracias
+            <?php 
+                if ($_SERVER['REQUEST_METHOD'] ==='POST'){
+                    echo htmlspecialchars($_POST['name']); 
+                }
+            ?>.
+        </h1>
+        <h2>Quieres conocer que estamos haciendo?</h2>
+        <p>Espero que te sientas mejor luego de compartir. Coloca tu correo aqui para que puedas que te compartamos lo que viene.</p>
+        <label>
+            <input type="text" placeholder="example@mail.com">
+        </label>
+        <button id="knowMore">
+            Tell Me More
+        </button>
+        <a href="/" class="vent-again">
+            I want to Vent Again
+        </a>
+        </div>
+    </main>
+    <footer>
+        <p>Made with ❤️ by <a href="https://mariaisabel.tech">Maria I.</a></p>
+        <p>Simple 🍂 <a href="privacy.html">Privacy Policy</a></p>
+    </footer>
+</body>
+</html>
